4734f8867b5bdb3367bec88e084193e81a014a24
faisal <faisal@afreespace.com>
2025-10-01 18:51:28 +0530

    Add Open Graph metadata for Sanket application
    - Added title, description, type, URL, and image meta tags to enhance SEO and social sharing for the Sanket CRM application.
    - Updated the layout component to include mobile-optimized meta tags for improved user experience across devices.

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,13 @@ export default async function RootLayout({
           }}
         />
         {/* Mobile-optimized meta tags */}
+        <title>Sanket</title>
+        <meta property="og:title" content="Sanket" />
+        <meta property="og:description"
+          content="CRM for beneficiary management" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://sanket.bridgeit.in/" />
+        <meta property="og:image" content="https://sanket.bridgeit.in/favicon/favicon-32x32.png" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
